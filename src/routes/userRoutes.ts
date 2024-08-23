1297f29fd613868aeeb28ebd1aa66fc7f2624dd6
Umakani <umakani310@gmail.com>
2024-08-23 12:47:46 +0530

    .env file testing

--- src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import getUserList from '../controllers/userController';
-
-const router = Router();
-
-router.get('/users', getUserList);
-
-export default router;
